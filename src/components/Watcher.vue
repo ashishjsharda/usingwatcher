d65e943159364dac48deb0deb9f36c940349c9ee
Ashish Sharda <ashishjsharda@gmail.com>
2022-12-29 06:39:10 -0600

    Add Watcher vue

---
+++ src/components/Watcher.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <h1>Shop Watcher</h1>
+
+    <div>
+      Black Friday sale
+      <strike>Was {{ oldDiscount }}%</strike>
+      <strong> Now {{ discount }}% OFF</strong>
+    </div>
+
+    <br />
+    <a href="#" @click="updateDiscount">Increase Discount!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      oldDiscount: 0,
+      discount: 5,
+    }
+  },
+  methods: {
+    updateDiscount() {
+      this.discount = this.discount + 5
+    },
+  },
+  watch: {
+    discount(newValue, oldValue) {
+      this.oldDiscount = oldValue
+    },
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
